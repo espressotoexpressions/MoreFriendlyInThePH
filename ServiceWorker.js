803104b88f02cef5a946cc9ebca08fd1ba0fe751
Karen <keannevillar@gmail.com>
2024-10-10 14:40:37 -0700

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "KarenCadavos-It's More Friendly in the Philippines Game-1.0";
+const contentToCache = [
+    "Build/ItsMoreFunInPHWebGame.loader.js",
+    "Build/ItsMoreFunInPHWebGame.framework.js",
+    "Build/ItsMoreFunInPHWebGame.data",
+    "Build/ItsMoreFunInPHWebGame.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
